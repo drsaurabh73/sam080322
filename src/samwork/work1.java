fe088722806ee74d2f380fd709e93deb490b8c90
drsaurabh73 <drsaurabh73@gmail.com>
2022-03-08 05:13:43 +0000

    sam080322

---
+++ src/samwork/work1.java
@@ -0,0 +1,7 @@
+package samwork;
+
+public class work1 {
+    public static void main (String [] args){
+        System.out.println("My name is Sam");
+    }
+}
---
+++ src/samwork/work2.java
@@ -0,0 +1,7 @@
+package samwork;
+
+public class work2 {
+    public static void main (String [] args){
+        System.out.println("I do Science work very quickly");
+    }
+}
